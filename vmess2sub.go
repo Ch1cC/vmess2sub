2a8ac19c03c33b759023960a37ebd4bd02bc343d
Ch1cC <949124624@qq.com>
2022-11-29 02:52:13 +0800

    v0.1.2 新增识别协议trojan

--- vmess2sub.go
+++ vmess2sub.go
@@ -6,40 +6,43 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	url2 "net/url"
 	"os"
 	"strings"
 )
 
-type vmess struct {
-	V    string `json:"v"`
-	Ps   string `json:"ps"`
-	Add  string `json:"add"`
-	Port string `json:"port"`
-	ID   string `json:"id"`
-	Aid  string `json:"aid"`
-	Scy  string `json:"scy"`
-	Net  string `json:"net"`
-	Type string `json:"type"`
-	Host string `json:"host"`
-	Path string `json:"path"`
-	TLS  string `json:"tls"`
-	Sni  string `json:"sni"`
-	Alpn string `json:"alpn"`
+type node struct {
+	Protocol string `json:"protocol"`
+	V        string `json:"v"`
+	Ps       string `json:"ps"`
+	Add      string `json:"add"`
+	Port     string `json:"port"`
+	ID       string `json:"id"`
+	Aid      string `json:"aid"`
+	Scy      string `json:"scy"`
+	Net      string `json:"net"`
+	Type     string `json:"type"`
+	Host     string `json:"host"`
+	Path     string `json:"path"`
+	TLS      string `json:"tls"`
+	Sni      string `json:"sni"`
+	Alpn     string `json:"alpn"`
 }
 
 type user struct {
-	ID   string `json:"id"`
+	ID    string `json:"id"`
 	Email string `json:"email"`
 }
 
 var (
-	vmessProtocol = "vmess://"
-	vmessPath     string
-	userPath      string
+	vmessProtocol  = "vmess://"
+	trojanProtocol = "trojan://"
+	vmessPath      string
+	userPath       string
 )
 
 func main() {
-	flag.StringVar(&vmessPath, "config", "vmess模板.json", "vmess模板json文件路径")
+	flag.StringVar(&vmessPath, "config", "vmess模板.json", "节点模板json文件路径")
 	flag.StringVar(&userPath, "user", "user模板.json", "user模板json文件路径")
 	/*vmessPath := ""
 	if len(os.Args) < 2 {
@@ -50,7 +53,7 @@ func main() {
 		vmessPath = os.Args[1]
 		fmt.Println("读取路径:", vmessPath)
 	}*/
-	vmesses := formatVmess()
+	nodes := formatNodes()
 	users := formatUser()
 	os.Mkdir("sub", 0644)
 	//循环vmess,user对象
@@ -59,15 +62,23 @@ func main() {
 		urlBuilder := strings.Builder{}
 		UUID := user.ID
 		email := user.Email
-		for _, vmess := range vmesses {
-			vmess.ID = UUID
-			marshal, err := json.Marshal(vmess)
+		for _, node := range nodes {
+			node.ID = UUID
+			protocol := node.Protocol
+			marshal, err := json.Marshal(node)
 			if err == nil {
 				//每个对象都进行base64转换
-				vmess := string(marshal)
-				base64Url := toVmess(vmess)
-				urlBuilder.WriteString(base64Url)
-				urlBuilder.WriteString("\n")
+				switch protocol {
+				case "trojan":
+					base64Url := toTrojan(node)
+					urlBuilder.WriteString(base64Url)
+					urlBuilder.WriteString("\n")
+				case "vmess":
+					vmess := string(marshal)
+					base64Url := toVmess(vmess)
+					urlBuilder.WriteString(base64Url)
+					urlBuilder.WriteString("\n")
+				}
 			}
 		}
 		vmessBuilder := urlBuilder.String()
@@ -83,18 +94,22 @@ func main() {
 	b := make([]byte, 1)
 	os.Stdin.Read(b)
 }
+func toTrojan(n node) (str string) {
+	url := trojanProtocol + n.ID + "@" + n.Add + ":" + n.Port + "?security=tls&alpn=http%2F1.1&type=tcp&headerType=none#" + url2.QueryEscape(n.Ps)
+	return url
+}
 
 func toVmess(str string) (base64Url string) {
 	return vmessProtocol + base64.StdEncoding.EncodeToString([]byte(str))
 }
 
-func formatVmess() []vmess {
-	vmessArr := make([]vmess, 0)
+func formatNodes() []node {
+	vmessArr := make([]node, 0)
 	vmess, _ := readJSON(vmessPath)
 	//获取json数组
 	JSONArr := json.Unmarshal(vmess, &vmessArr)
 	if JSONArr != nil {
-		fmt.Println("vmess模板.json is error")
+		fmt.Println("节点模板.json is error")
 		fmt.Println("回车退出")
 		b := make([]byte, 1)
 		os.Stdin.Read(b)
